fc3f4627cce3c249481036000ba1959b0d8fdc49
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-03-26 14:06:53 +0800

    sonar-scanner-cli-4.6.2.2472

---
+++ it/src/test/java/com/sonarsource/scanner/it/SonarScannerTestSuite.java
@@ -0,0 +1,47 @@
+/*
+ * SonarSource :: IT :: SonarScanner CLI
+ * Copyright (C) 2009-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.sonarsource.scanner.it;
+
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.locator.MavenLocation;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ScannerTest.class, MultimoduleTest.class,
+                DistributionTest.class})
+public class SonarScannerTestSuite {
+
+  @ClassRule
+  public static final Orchestrator ORCHESTRATOR = createOrchestrator();
+
+  private static Orchestrator createOrchestrator() {
+    String sonarVersion = System
+      .getProperty("sonar.runtimeVersion", "LATEST_RELEASE[7.9]");
+    return Orchestrator.builderEnv()
+      .setSonarVersion(
+        sonarVersion).addPlugin(MavenLocation
+        .of("org.sonarsource.sonarqube", "sonar-xoo-plugin",
+          sonarVersion)).build();
+  }
+
+}
---
+++ src/main/java/org/sonarsource/scanner/cli/ScannerFactory.java
@@ -0,0 +1,67 @@
+/*
+ * SonarScanner CLI
+ * Copyright (C) 2011-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.scanner.cli;
+
+import java.util.Map;
+import java.util.Properties;
+import org.sonarsource.scanner.api.EmbeddedScanner;
+import org.sonarsource.scanner.api.LogOutput;
+
+class ScannerFactory {
+
+  private final Logs logger;
+
+  public ScannerFactory(Logs logger) {
+    this.logger = logger;
+  }
+
+  EmbeddedScanner create(Properties props, String isInvokedFrom) {
+    String appName = "ScannerCLI";
+    String appVersion = ScannerVersion.version();
+    if (!isInvokedFrom.equals("") && isInvokedFrom.contains("/")) {
+      appName = isInvokedFrom.split("/")[0];
+      appVersion = isInvokedFrom.split("/")[1];
+    }
+
+    return EmbeddedScanner.create(appName, appVersion, new DefaultLogOutput())
+      .addGlobalProperties((Map) props);
+  }
+
+  class DefaultLogOutput implements LogOutput {
+    @Override
+    public void log(String formattedMessage, Level level) {
+      switch (level) {
+        case TRACE:
+        case DEBUG:
+          logger.debug(formattedMessage);
+          break;
+        case ERROR:
+          logger.error(formattedMessage);
+          break;
+        case WARN:
+          logger.warn(formattedMessage);
+          break;
+        case INFO:
+        default:
+          logger.info(formattedMessage);
+      }
+    }
+  }
+}
